67f40ef8f717a350c273a943455efeb99360a6f1
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2025-11-26 09:13:53 +0100

    Removed img in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,16 +10,11 @@ function About() {
             <p> Under vårterminen 2024 tog jag tjänstledigt och började studera kurser inom IT och programmering på egen hand. Jag läste då bland annat webbutveckling, objektorienterad programmering och Linux. Under den tiden upptäckte jag att det är systemutveckling jag vill ägna mig åt. Jag tog en paus i studierna och var föräldraledig i ett år. När det började bli dags att ta upp studierna igen bestämde jag mig för att läsa en YH-utbildning istället och påbörjade programmet Webbutveckling inom .NET hösten 2025 och är väldigt nöjd med mitt val!</p>
 
             <div id="about-wrapper">
-                <div>
+            
                 <p>  För övrigt är jag en väldigt analyserande person, grottar gärna ner mig i ämnen och är ständigt på jakt efter nya, intressanta saker att lära mig. Jag skulle säga att jag är en problemlösare och tar mig gärna an utmaningar.
-                    På min fritid umgås jag mycket med min familj, tycker om styrketräning och att vara ute i naturen.  </p>
-
+                    På min fritid umgås jag mycket med min familj, tycker om styrketräning och att vara ute i naturen.  </p> 
                 <p>   Hoppas vi hörs!   </p>
-                </div>
-
-                <img className="about-img" src={about} alt="" />
-
-
+            
             </div>
 
             <hr className="divider"></hr>
